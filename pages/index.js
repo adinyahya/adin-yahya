fee9b7cad28e0f25ca60ae3a876ec691fd69a826
haloden <yahyaadin@gmail.com>
2021-01-09 15:21:00 +0700

    i

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h3>
-        Hai :)
-        </h3>
+        
         {/* <div className={styles.w} id='textDestination'>
 	
       </div>  */}
