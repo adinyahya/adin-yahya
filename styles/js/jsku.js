7f687e37be0df044f80be02b02eefd725a54ba23
haloden <yahyaadin@gmail.com>
2021-01-09 14:20:45 +0700

    p

---
+++ styles/js/jsku.js
@@ -0,0 +1,35 @@
+
+var text="Hello, I'm Adin Yahya. A Newbie Software Developer.";
+var text2="A Newbie Software Developer";
+var delay=19;
+var currentChar=1;
+var destination="[none]";
+	function type()
+	{
+		{
+			var dest=document.getElementById(destination);
+			if (dest)
+			{
+				dest.innerHTML=text.substr(0, currentChar)+"<blink>_</blink>";
+				currentChar++;
+					if (currentChar>text.length)
+					{
+						currentChar=1;
+						setTimeout("type()", exit(0));
+					}
+					else
+					{
+					setTimeout("type()", delay);
+					}
+			}
+		}
+	}
+
+	function startTyping(textParam, delayParam, destinationParam)
+	{
+				text=textParam;
+				delay=delayParam;
+				currentChar=1;
+				destination=destinationParam;
+				type();
+	}
