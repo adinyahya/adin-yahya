696d0baf11b9ce2424da35ac536e7d5c28a14333
haloden <yahyaadin@gmail.com>
2021-01-15 20:41:58 +0700

    push

--- pages/hello.js
+++ pages/hello.js
@@ -76,13 +76,13 @@ export default function Index() {
               size="xl"
               visibility={['visible', 'visible', 'hidden']}
             >
-              Hi! I'm Adin Yahya, a newbie developer.
+              Hai! I'm Adin Yahya, a newbie developer.
             </Heading>
             <Text
               color={colorSecondary[colorMode]}
               mt={[0, 0, 12]}
             >
-              I’m a developer and creator. On this site you will find
+              I’m a newbie developer and creator. On this site you will find
             my projects, gear, writing, and much more. Feel free to reach out via <Link href="mailto:hai@adinyahya.com" color="pink.500">email</Link> or any social media to say hai!
             </Text>
 
@@ -166,7 +166,7 @@ export default function Index() {
                 src="/images/o.png"
                 color={iconColor[colorMode]}
             >
-                Building Employee Performance Achievement System Website Dinas Perhubungan Kabupaten Jombang.
+                Building a Website Employee Performance Achievement System Dinas Perhubungan Kabupaten Jombang.
             </ FeaturedProjectCard>
             <FeaturedProjectCard
                 title="Dinas Kependudukan Pencatatan Sipil Kabupaten Jombang"
@@ -174,7 +174,7 @@ export default function Index() {
                 src="/images/o.png"
                 color={iconColor[colorMode]}
             >
-                Building Employee Performance Measurement System Website Dinas Kependudukan Dan Pencatatan Sipil Kabupaten Jombang.
+                Building a Website Employee Performance Measurement System Dinas Kependudukan Dan Pencatatan Sipil Kabupaten Jombang.
             </ FeaturedProjectCard>
             <FeaturedProjectCard
                 title="Kecamatan Peterongan Kabupaten Jombang"
@@ -182,7 +182,7 @@ export default function Index() {
                 src="/images/o.png"
                 color={iconColor[colorMode]}
             >
-                Build a website for Monitoring Employee Performance Evaluation Results Kecamatan Peterongan Kabupaten Jombang.
+                Building a Website for Monitoring Employee Performance Evaluation Results Kecamatan Peterongan Kabupaten Jombang.
                 {/* My personal portfolio website you are on now. Built with <Code>Next.js</Code>, <Code>chakra-ui</Code>, <Code>mdx</Code> pages, and serverless functions for realtime data. */}
             </FeaturedProjectCard>
             
--- pages/projects.js
+++ pages/projects.js
@@ -74,7 +74,7 @@ const Projects = () => {
                         src="/images/o.png"
                         color={iconColor[colorMode]}
                         >
-                            Building Employee Performance Achievement System Website Dinas Perhubungan Kabupaten Jombang.
+                            Building a Website Employee Performance Achievement System Dinas Perhubungan Kabupaten Jombang.
                         </ FeaturedProjectCard>
                         <FeaturedProjectCard
                             title="Dinas Kependudukan Pencatatan Sipil Kabupaten Jombang"
@@ -82,7 +82,7 @@ const Projects = () => {
                             src="/images/o.png"
                             color={iconColor[colorMode]}
                         >
-                            Building Employee Performance Measurement System Website Dinas Kependudukan Dan Pencatatan Sipil Kabupaten Jombang.
+                            Building a Website Employee Performance Measurement System Dinas Kependudukan Dan Pencatatan Sipil Kabupaten Jombang.
                         </ FeaturedProjectCard>
                         <FeaturedProjectCard
                             title="Kecamatan Peterongan Kabupaten Jombang"
@@ -90,7 +90,7 @@ const Projects = () => {
                             src="/images/o.png"
                             color={iconColor[colorMode]}
                         >
-                            Build a website for Monitoring Employee Performance Evaluation Results Kecamatan Peterongan Kabupaten Jombang.
+                            Building a Website for Monitoring Employee Performance Evaluation Results Kecamatan Peterongan Kabupaten Jombang.
                             {/* My personal portfolio website you are on now. Built with <Code>Next.js</Code>, <Code>chakra-ui</Code>, <Code>mdx</Code> pages, and serverless functions for realtime data. */}
                         </FeaturedProjectCard>
                         <FeaturedProjectCard
